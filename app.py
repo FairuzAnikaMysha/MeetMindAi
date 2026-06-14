dbd96f9fd971c3cc70793e1dceb05b0f785b4208
FairuzAnikaMysha <148949978+FairuzAnikaMysha@users.noreply.github.com>
2026-06-15 07:28:58 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,101 @@
+
+import streamlit as st
+import google.generativeai as genai
+import pdfplumber
+
+from report_generator import generate_pdf
+
+# API KEY
+genai.configure(api_key="YOUR_GEMINI_API_KEY")
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+st.set_page_config(
+    page_title="MeetMind AI",
+    page_icon="🧠",
+    layout="wide"
+)
+
+st.title("🧠 MeetMind AI")
+st.caption("AI Powered Meeting Intelligence Assistant")
+
+uploaded_file = st.file_uploader(
+    "Upload Meeting Transcript",
+    type=["pdf","txt"]
+)
+
+meeting_notes = st.text_area(
+    "Or Paste Meeting Notes",
+    height=250
+)
+
+text = ""
+
+if uploaded_file:
+
+    if uploaded_file.name.endswith(".pdf"):
+
+        with pdfplumber.open(uploaded_file) as pdf:
+
+            for page in pdf.pages:
+
+                page_text = page.extract_text()
+
+                if page_text:
+                    text += page_text
+
+    else:
+
+        text = str(uploaded_file.read(),"utf-8")
+
+if meeting_notes:
+    text += meeting_notes
+
+if st.button("Analyze Meeting"):
+
+    if text.strip() == "":
+        st.warning("Please upload or enter meeting notes.")
+        st.stop()
+
+    prompt = f"""
+You are a professional business analyst.
+
+Analyze the meeting transcript.
+
+Return:
+
+1. Executive Summary
+
+2. Key Decisions
+
+3. Action Items
+
+4. Task Owners
+
+5. Deadlines
+
+6. Follow-up Email
+
+Meeting Notes:
+
+{text}
+"""
+
+    with st.spinner("Analyzing Meeting..."):
+
+        response = model.generate_content(prompt)
+
+        result = response.text
+
+    st.session_state["report"] = result
+
+    st.markdown(result)
+
+    pdf_file = generate_pdf(result)
+
+    st.download_button(
+        label="Download Report",
+        data=pdf_file,
+        file_name="meeting_report.pdf",
+        mime="application/pdf"
+    )
