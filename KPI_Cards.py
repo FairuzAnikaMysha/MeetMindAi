114986d8cbf89c3f10fd15024d58e32f0513f919
FairuzAnikaMysha <148949978+FairuzAnikaMysha@users.noreply.github.com>
2026-06-15 07:32:43 +0800

    Add files via upload

---
+++ KPI_Cards.py
@@ -0,0 +1,16 @@
+col1,col2,col3 = st.columns(3)
+
+col1.metric(
+    "Meetings Analyzed",
+    "125"
+)
+
+col2.metric(
+    "Tasks Extracted",
+    "578"
+)
+
+col3.metric(
+    "Time Saved",
+    "310 Hours"
+)
